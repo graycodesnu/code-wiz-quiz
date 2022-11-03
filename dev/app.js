3d5934be6f5c94164558a6959b23f5d75e6fa7d5
Grayson Harvey <graycodes.nu@gmail.com>
2022-11-02 23:35:00 -0500

    finally got timer working! need to work through line 133 error next.

--- dev/app.js
+++ dev/app.js
@@ -37,162 +37,162 @@ var quizQuestions = [
 ];
 
 // TODO: Timer refactor
-const time = 60;
-timer;
+var time = 60;
+var timer;
 console.log("timer", timer);
-const display = document.querySelector("#time");
+var display = document.querySelector("#time");
 
 //! Local storage
 if (!localStorage.getItem("highScores")) {
   localStorage.setItem("highScores", JSON.stringify([]));
 }
 
-//* ******** Landing Page ********
+//* ******** Quiz Wiz Page ********
 
-if (window.location.pathname === "/code-wiz-quiz/index.html") {
-  //! Start button
-  const startButton = document.getElementById("startButton");
-  startButton.addEventListener("click", startQuiz);
-
-  function startQuiz() {
-    const startMessage = document.getElementById("startingQuiz");
-    startMessage.style.display = "none";
-    timer = setInterval(startTimer, 1000);
-    displayQ();
+//! Display high scores if on quizWiz page
+if (window.location.pathname === "/code-wiz-quiz/quizWizzes.html") {
+  let scores = JSON.parse(localStorage.getItem("highScores"));
+  let scoreBoard = document.querySelector("#scoreBoard");
+  for (let i = 0; i < scores.length; i++) {
+    let liEl = document.createElement("li");
+    liEl.textContent = scores[i].name + " ** " + scores[i].score;
+    scoreBoard.appendChild(liEl);
   }
 
-  function startTimer() {
-    time = time = -1;
-    display.textContent = time;
-    if (time <= 0) {
-      quizComplete();
-      alert("Out of time!");
+  // Define clear and back buttons 
+  var clearBtn = document.getElementById("clearHighScores");
+  var backBtn = document.getElementById("backButton");
+
+  //! Reset quizWiz high scores
+  if (clearBtn) {
+    clearBtn.addEventListener("click", clearScores);
+    function resetScores() {
+      localStorage.setItem("highScores", jSON.stringify([]));
+      console.log("resetting scoreboard");
+      let scoreBoardList = document.getElementById("scoreBoard");
+      scoreBoardList.innerHTML = "";
     }
   }
 
-  function displayQ() {
-    if (currentQIndex >= 4) {
-      return quizComplete();
-    }
-    currentQIndex++;
-    console.log("currentQIndex", currentQIndex);
-  }
-
-  //! Call questions
-  const currentQ = quizQuestions[currentQIndex];
-
-  const dataTypes = document.createElement("h1");
-  const listEl = document.createElement("ol");
-  listEl.addEventListener("click", function (e) {
-    checkChoice(e);
-  });
-  const li1 = document.createElement("li");
-  li1.setAttribute("id", "choice1");
-  const li2 = document.createElement("li");
-  li2.setAttribute("id", "choice2");
-  const li3 = document.createElement("li");
-  li3.setAttribute("id", "choice3");
-  const li4 = document.createElement("li");
-  li4.setAttribute("id", "choice4");
-
-  dataTypes.textContent = currentQ.question;
-  li1.textContent = currentQ.answers[0];
-  li2.textContent = currentQ.answers[1];
-  li3.textContent = currentQ.answers[2];
-  li4.textContent = currentQ.answers[3];
-
-  const questionContainer = document.getElementById("questionContainer");
-  questionContainer.innerHTML = "";
-  questionContainer.appendChild(dataTypes);
-  dataTypes.appendChild(listEl);
-  listEl.appendChild(li1);
-  listEl.appendChild(li2);
-  listEl.appendChild(li3);
-  listEl.appendChild(li4);
-
-  //! Verify user's selection, count correct questions
-  const correctQCount = 0;
-
-  function verifyQ(e) {
-    let currentQ = quizQuestions[currentQIndex];
-    let rightChoiceIndex = currentQ.correctAnswer;
-    let rightChoiceContent = currentQ.answers[rightChoiceIndex];
-    let result = document.createElement("p");
-    const questionContainer = document.getElementById("questionContainer");
-    questionContainer.appendChild(result);
-
-    if (e.target.textContent === rightChoiceContent) {
-      correctQCount++;
-      result.textContent = "Right on!";
-    } else {
-      result.textContent = "Try again!";
-      time -= 10;
-    }
-    setTimeout(displayQ, 1000);
-  }
-
-  function quizComplete() {
-    clearInterval(timer);
-    const score = document.querySelector(".score");
-
-    const completedEl = document.querySelector("completed");
-    completedEl.style.display = "block";
-
-    const hideQs = document.querySelector("#questionContainer");
-    hideQs.style.display = "none";
-    score.textContent = `Final score: ${time}`;
-
-    quizWizzes();
-  }
-
-  //! Submit high scores
-  function quizWizzes() {
-    const submitScore = document.querySelector("#submitBtn");
-    submitScore.addEventListener("click", () => {
-      let inputInitials = document.querySelector("#inputInitials");
-      let initials = inputInitials.value;
-      let previousScores = JSON.parse(localStorage.getItem("highScores"));
-      previousScores.push({
-        name: initials,
-        score: time,
-      });
-      localStorage.setItem("highScores", JSON.stringify(previousScores));
-      window.open("./quizWizzes.html");
+  //! Back to quiz home
+  if (backBtn) {
+    backBtn.addEventListener("click", () => {
+      window.open("./index.html");
     });
   }
 } else {
-  // Close window path function
 
-  //* ******** Quiz Wiz Page ********
+//* ******** Landing Page ********
+  //! Start button
+    var startButton = document.getElementById("startButton");
+    startButton.addEventListener("click", startQuiz);
 
-  //! Display high scores if on quizWiz page
-  if (window.location.pathname === "/code-wiz-quiz/quizWizzes.html") {
-    let scores = JSON.parse(localStorage.getItem("highScores"));
-    let scoreBoard = document.querySelector("#scoreBoard");
-    for (let i = 0; i < scores.length; i++) {
-      let liEl = document.createElement("li");
-      liEl.textContent = scores[i].name + " ** " + scores[i].score;
-      scoreBoard.appendChild(liEl);
+    function startQuiz() {
+      var startMessage = document.getElementById("startingQuiz");
+      startMessage.style.display = "none";
+      timer = setInterval(startTimer, 1000);
+      displayQ();
     }
-    var clearBtn = document.getElementById("clearHighScores");
-    var backBtn = document.getElementById("backBtn");
 
-    //! Reset quizWiz high scores
-    if (clearBtn) {
-      clearBtn.addEventListener("click", clearScores);
-      function resetScores() {
-        localStorage.setItem("highScores", jSON.stringify([]));
-        console.log("resetting scoreboard");
-        let scoreBoardList = document.getElementById("scoreBoard");
-        scoreBoardList.innerHTML = "";
+    //! Start Timer
+    function startTimer() {
+      time = time - 1;
+      display.textContent = time;
+      if (time <= 0) {
+        quizComplete();
+        // alert("Out of time!");
       }
     }
-    //! Back to quiz home
-    if (backBtn) {
-      backBtn.addEventListener("click", () => {
-        window.open("./index.html");
+
+    //! Call index and display questions 
+    function displayQ() {
+      if (currentQIndex >= 4) {
+        return quizComplete();
+      }
+      currentQIndex++;
+      console.log("currentQIndex", currentQIndex);
+    }
+
+    //! Call questions
+    var currentQ = quizQuestions[currentQIndex];
+
+    var dataTypes = document.createElement("h1");
+    var listEl = document.createElement("ol");
+    listEl.addEventListener("click", function (e) {
+      checkChoice(e);
+    });
+    var li1 = document.createElement("li");
+    li1.setAttribute("id", "choice1");
+    var li2 = document.createElement("li");
+    li2.setAttribute("id", "choice2");
+    var li3 = document.createElement("li");
+    li3.setAttribute("id", "choice3");
+    var li4 = document.createElement("li");
+    li4.setAttribute("id", "choice4");
+
+    dataTypes.textContent = currentQ.question;
+    li1.textContent = currentQ.answers[0];
+    li2.textContent = currentQ.answers[1];
+    li3.textContent = currentQ.answers[2];
+    li4.textContent = currentQ.answers[3];
+
+    var questionContainer = document.getElementById("questionContainer");
+    questionContainer.innerHTML = "";
+    questionContainer.appendChild(dataTypes);
+    dataTypes.appendChild(listEl);
+    listEl.appendChild(li1);
+    listEl.appendChild(li2);
+    listEl.appendChild(li3);
+    listEl.appendChild(li4);
+
+    //! Verify user's selection, count correct questions
+    var correctQCount = 0;
+
+    function verifyQ(e) {
+      let currentQ = quizQuestions[currentQIndex];
+      let rightChoiceIndex = currentQ.correctAnswer;
+      let rightChoiceContent = currentQ.answers[rightChoiceIndex];
+      let result = document.createElement("p");
+      var questionContainer = document.getElementById("questionContainer");
+      questionContainer.appendChild(result);
+
+      if (e.target.textContent === rightChoiceContent) {
+        correctQCount++;
+        result.textContent = "Right on!";
+      } else {
+        result.textContent = "Try again!";
+        time -= 10;
+      }
+      setTimeout(displayQ, 1000);
+    }
+
+    function quizComplete() {
+      clearInterval(timer);
+      var score = document.querySelector(".score");
+
+      var completedEl = document.querySelector(".completed");
+      completedEl.style.display = "block";
+
+      var hideQs = document.querySelector("#questionContainer");
+      hideQs.style.display = "none";
+      score.textContent = `Final score: ${time}`;
+
+      quizWizzes();
+    }
+
+    //! Submit high scores
+    function quizWizzes() {
+      var submitScore = document.querySelector("#submitButton");
+      submitScore.addEventListener("click", () => {
+        let inputInitials = document.querySelector("#inputInitials");
+        let initials = inputInitials.value;
+        let previousScores = JSON.parse(localStorage.getItem("highScores"));
+        previousScores.push({
+          name: initials,
+          score: time,
+        });
+        localStorage.setItem("highScores", JSON.stringify(previousScores));
+        window.open("./quizWizzes.html");
       });
     }
-    // Close high scores window path function
   }
-}
